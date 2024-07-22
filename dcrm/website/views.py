e024988bdbe44d4d557ece8cbf842fac52d227bc
Mehul Suthar <84217273+MehulSuthar-000@users.noreply.github.com>
2024-07-22 17:21:26 +0530

    added login and logout functionalities

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 def home(request):
-    return render(request, 'website/home.html', {})
+    # Check to see if logging in
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f"You have been logged lin")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error logging in, please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'website/home.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out...")
+    return redirect('home')
+
