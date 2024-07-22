cdeefb97c6064f52886ac7705146a571530e3faf
Mehul Suthar <84217273+MehulSuthar-000@users.noreply.github.com>
2024-07-22 19:12:11 +0530

    Fixed the registeration bug

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -25,3 +26,22 @@ def logout_user(request):
     messages.success(request, "You Have Been Logged Out...")
     return redirect('home')
 
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and login
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome")
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+        context = {'form':form}
+        return render(request, 'website/register.html', context)
+    return render(request, 'website/register.html', {'form':form})
